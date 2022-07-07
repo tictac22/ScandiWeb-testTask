458b4efe7497af06facf9c10a0bc3e857fcc6983
tictac <fominsartems947@gmail.com>
2022-07-07 21:27:05 +0300

    removed unused function

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -7,18 +7,6 @@ export const getCurrentPrice = (labelPrice: string, priceArr: Prices[]) => {
 	return `${currency.currency.symbol + formatedPrice}`
 }
 
-export const createText = (attributes: State[]) => {
-	const notSelected = attributes.filter((item) => !item.selected)
-	const string = notSelected.reduce((prev, current, index, array) => {
-		return (
-			prev +
-			current.name +
-			(array.length - 2 === index ? " and " : "") +
-			(array.length - 1 === index || array.length - 2 === index ? "" : ", ")
-		)
-	}, "")
-	return `Please select: ${string}`
-}
 export const addPaging = (isActive: boolean) => {
 	const scrollWidth = window.innerWidth - document.documentElement.clientWidth
 	const pageBody = document.body
@@ -29,16 +17,4 @@ export const addPaging = (isActive: boolean) => {
 		pageBody.style.paddingRight = `${scrollWidth}px`
 		pageBody.classList.add("_lock")
 	}
-}
-
-interface test {
-	displayValue: string
-	value: string
-}
-interface State {
-	name: string
-	selected: boolean
-	value: string
-	type: string
-	items: test[]
 }
